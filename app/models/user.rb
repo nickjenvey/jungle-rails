7f1145ec843da52b63016d14313f2e5be3240b2d
Nick <nbjenvey@gmail.com>
2019-07-11 16:40:42 +0000

    Order details working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates_uniqueness_of :email, case_sensitive: false, presence: true
+  validates :password, presence: true
+  validates :password, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+  validates :first_name, :last_name, presence: true
+
 end
