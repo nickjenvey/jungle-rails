061c81b13538f17fbe6d22033e394ba0aa256b67
Nick <nbjenvey@gmail.com>
2019-07-12 20:51:06 +0000

    Rating feature fully working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,22 @@ class ReviewsController < ApplicationController
   before_filter :authorize
 
   def create
-    raise "Yay, I'm here!"
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.product = Product.find(params[:product_id])
+
+    if @review.valid?
+      @review.save
+      redirect_to product_url(params[:product_id])
+    else 
+      redirect_to product_url(params[:product_id])
+    end
+  end
+
+  def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to product_url(params[:product_id])
   end
 
   private
